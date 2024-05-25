33bb01e38329df6dc04272dd4924de426eaacdfe
Brian <67478505+MrAnderson1971@users.noreply.github.com>
2024-05-24 20:19:33 -0700

    refactor
    Update bot.py

--- bot.py
+++ bot.py
@@ -1,144 +1,145 @@
 import os
 import sys
-
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import pickle
 import asyncio
 
+# Load environment variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+# Command prefix
 PREFIX = "?"
 
-client = discord.Client()
-permissions = discord.Permissions()
+# Initialize the bot with command prefix
+bot = commands.Bot(command_prefix=PREFIX)
 
-try:
-    with open('channels.txt', 'rb') as f:
-        channels = pickle.load(f)
-except:
-    channels = {}
+# Load or initialize channels data
+def load_channels(filename='channels.txt'):
+    try:
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
+    except (FileNotFoundError, EOFError):
+        return {}
 
-@client.event
+def save_channels(channels, filename='channels.txt'):
+    with open(filename, 'wb') as f:
+        pickle.dump(channels, f)
+
+channels = load_channels()
+
+# Event when the bot is ready
+@bot.event
 async def on_ready():
-    print(
-        f'{client.user} is connected to the following guild:\n')
-    for guild in client.guilds:
-        if guild.id not in channels.keys():
+    print(f'{bot.user} is connected to the following guilds:')
+    for guild in bot.guilds:
+        if guild.id not in channels:
             channels[guild.id] = set()
-        
+        print(f'{guild.name} (id: {guild.id})')
+        member = guild.get_member(825113715464208404)
+        if member:
+            await member.edit(nick="Connor")
 
-   
-        print(f'{guild.name}(id: {guild.id})\n')
-        await guild.get_member(825113715464208404).edit(nick="Connor")
+# Command: add channels to the list
+@bot.command(name='add')
+@commands.has_permissions(manage_channels=True)
+async def add_channels(ctx, *channel_mentions):
+    guild_channels = channels[ctx.guild.id]
+    for channel in ctx.message.channel_mentions:
+        guild_channels.add(channel.id)
+        await ctx.send(f'Added: <#{channel.id}>')
+    save_channels(channels)
 
-@client.event
+# Command: remove channels from the list
+@bot.command(name='remove')
+@commands.has_permissions(manage_channels=True)
+async def remove_channels(ctx, *channel_mentions):
+    guild_channels = channels[ctx.guild.id]
+    for channel in ctx.message.channel_mentions:
+        if channel.id in guild_channels:
+            guild_channels.remove(channel.id)
+            await ctx.send(f'Removed: <#{channel.id}>')
+    save_channels(channels)
+
+# Command: lockdown channels
+@bot.command(name='lockdown')
+@commands.has_permissions(manage_channels=True)
+async def lockdown_channels(ctx):
+    guild_channels = channels[ctx.guild.id]
+    for channel_id in guild_channels:
+        channel = bot.get_channel(channel_id)
+        if channel:
+            await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+            await ctx.send(f'Locked down: <#{channel_id}>')
+
+# Command: lift lockdown on channels
+@bot.command(name='lift')
+@commands.has_permissions(manage_channels=True)
+async def lift_lockdown(ctx):
+    guild_channels = channels[ctx.guild.id]
+    for channel_id in guild_channels:
+        channel = bot.get_channel(channel_id)
+        if channel:
+            await channel.set_permissions(ctx.guild.default_role, send_messages=None)
+            await ctx.send(f'Unlocked: <#{channel_id}>')
+
+# Command: list added channels
+@bot.command(name='list')
+@commands.has_permissions(manage_channels=True)
+async def list_channels(ctx):
+    guild_channels = channels[ctx.guild.id]
+    await ctx.send(f'Channels added for unlocking in {ctx.guild.name}:')
+    for channel_id in guild_channels:
+        await ctx.send(f'<#{channel_id}>')
+
+# Command: clear all channels from the list
+@bot.command(name='clear')
+@commands.has_permissions(manage_channels=True)
+async def clear_channels(ctx):
+    channels[ctx.guild.id] = set()
+    await ctx.send(f'Cleared all channels for {ctx.guild.name}')
+    save_channels(channels)
+
+# Command: send a file
+@bot.command(name='file')
+async def send_file(ctx, filename: str):
+    await ctx.message.delete()
+    try:
+        await ctx.send(file=discord.File(filename))
+    except FileNotFoundError:
+        await ctx.send(f'File not found: {filename}')
+
+# Command: exit the bot
+@bot.command(name='exit')
+@commands.is_owner()
+async def exit_bot(ctx):
+    await ctx.message.add_reaction('\U0001f44b')
+    await ctx.bot.close()
+    sys.exit()
+
+# Custom reply based on guild name
+@bot.event
 async def on_message(message):
-    cmd = message.content.split()
-    guild_channels = channels[message.guild.id]
-    guild = message.guild
+    if message.author == bot.user:
+        return
 
-    if f'<@!{client.user.id}>' in message.content:
-        if guild.name == "FREN 101":
+    if f'<@!{bot.user.id}>' in message.content:
+        if message.guild.name == "FREN 101":
             await message.channel.send(f"Bonjour <@!{message.author.id}>. Je m'appelle Connor. Je suis l'androïde envoyé par Cyberlife.\nMa mission est de réussir mes examens. Et j'accomplis toujours ma mission.")
         else:
             await message.channel.send(f'Hello <@!{message.author.id}>. My name is Connor. I\'m the android sent by Cyberlife.\nMy mission is to pass my exams. And I always accomplish my mission.')
-            #await message.channel.send(f"你好 <@!{message.author.id}>。我叫 Connor。我是Cyberlife送来的机器人。\n我的任务是通过考试。而且，我的任务每次成功。")
-            #await message.channel.send(f"Salve <@!{message.author.id}>. Meum nomen est Connor. Android sum missus a Cyberlife.\nMy missionem is to pass my probationes. Et semper missionem meam perficio.")
 
-    if message.author == client.user:
-        return
+    # Process commands
+    await bot.process_commands(message)
 
-
-    if cmd[0][0] != PREFIX: return
-
-    cc = cmd[0].strip(PREFIX)
-
-    if not message.channel.permissions_for(message.author).manage_channels:
-        print('%s attempted to run:\n%s' % (message.author, message.content))
-
-        await message.channel.send("Manage channel permissions required to execute this command!")
-        return
-
-    print('%s:\n%s' % (message.author, message.content))
-
-
-    if cc == "add":
-        await message.channel.send('Added the following:')
-        for channel in message.raw_channel_mentions:
-            guild_channels.add(channel)
-            await message.channel.send(f'<#{channel}>')
-    
-
-    elif cc == "remove":
-        await message.channel.send('Removed the following:')
-        for channel in message.raw_channel_mentions:
-            if channel in guild_channels:
-                guild_channels.remove(channel)
-                await message.channel.send(f'<#{channel}>')
-
-    elif cc == "lockdown":
-        await message.channel.send('Locking:')
-        for channel in guild_channels:
-            await client.get_channel(channel).set_permissions(message.guild.default_role, send_messages=False)
-            await message.channel.send(f'Locked down <#{channel}>')
-
-    elif cc == "lift":
-        await message.channel.send('Unlocking:')
-        for channel in guild_channels:
-            await client.get_channel(channel).set_permissions(message.guild.default_role, send_messages=None)
-            await message.channel.send(f'Unlocked <#{channel}>')
-
-    elif cc == "list":
-        await message.channel.send('Channels added to unlocking for %s:' % message.guild)
-        for channel in guild_channels:
-            await message.channel.send('<#%s>' % channel)
-
-    elif cc == "all":
-        await message.channel.send('All servers:')
-        for guild in channels.keys():
-            await message.channel.send("%s:" % guild)
-            for channel in channels[guild]:
-                await message.channel.send(f'<#{channel}>')
-
-    elif cc == "clear":
-        await message.channel.send('Clearing lockdowns for %s...' % message.guild.name)
-        channels[message.guild.id] = set()
-
-    elif cc == 'exit':
-        await message.add_reaction('\U0001f44b')
-        sys.exit()
-
-    elif cc == 'file':
-        await message.delete()
-        try:
-            await message.channel.send(file=discord.File(' '.join(cmd[1:])))
-        except FileNotFoundError as e:
-            print(e)
-
-    elif cc == 'temp':
-        '''messages = await message.channel.history(limit=1000).flatten()
-        for m in messages:
-            print('Deleted %s' % m.content)
-            await m.delete()'''
-        '''for i in range(101, 114):
-            await guild.create_role(name="Section %s" % i)'''
-        for role in guild.roles:
-            if role.name.startswith("Lab"):
-                print(role.name)
-                await role.delete()
-
-    else:
+    # Default case: repeat the message if it doesn't match any command
+    if message.content.startswith(PREFIX) and not any(cmd.name in message.content for cmd in bot.commands):
         contents = message.content
         await message.delete()
         await message.channel.send(contents.strip(PREFIX))
 
-    save('channels.txt')
-            
-def save(file):
-    with open(file, 'wb') as f:
-        pickle.dump(channels, f)
-    
-client.run(TOKEN)
+# Run the bot
+bot.run(TOKEN)
